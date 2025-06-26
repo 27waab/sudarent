491830cdebb000bb2be9aa99334edb47e9896f7e
Awab Abdelbagi <awaababdelbagi@gmail.com>
2025-06-26 21:48:30 +0200

    Add files via upload

---
+++ JS/translate.js
@@ -0,0 +1,14 @@
+const translations = {
+    ar: {
+        pageTitle: "الميزان العقاري",
+        title: "الميزان العقاري",
+        description: "منصة 'الميزان العقاري' تمنحك تقديرًا منصفًا لسعر الإيجار استنادًا إلى مواصفات العقار وموقعه، ليساعدك على الوصول إلى سعر عادل ومناسب للطرفين بسهولة وسرعة.",
+        nextBtn: "ابدأ الحساب"
+    },
+    en: {
+        pageTitle: "Real Estate Scale",
+        title: "Real Estate Scale",
+        description: "The Mizan Real Estate platform provides you with a fair estimate of the rental price based on the property's specifications and location, helping you reach a fair and mutually acceptable price easily and quickly.",
+        nextBtn: "Start Calculation"
+    }
+};
