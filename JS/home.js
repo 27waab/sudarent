f9e9ddbb7d294a68d20096ef738e9f365331ee29
Awab Abdelbagi <awaababdelbagi@gmail.com>
2025-06-30 11:55:15 +0300

    Update home.js

--- JS/home.js
+++ JS/home.js
@@ -9,12 +9,14 @@ langBtn.addEventListener("click", () => {
         window.document.dir = "ltr";
         changeLangugeText.style.fontSize = "18px";
         changeLangugeText.style.fontFamily = "IBM Plex Sans Arabic";
+        langBtn.style.width = "130px";
     } else if (currentLang === "en") {
         currentLang = "ar";
         document.body.style.fontFamily = "IBM Plex Sans Arabic";
         window.document.dir = "rtl";
         changeLangugeText.style.fontSize = "14px";
         changeLangugeText.style.fontFamily = "Poppins";
+        langBtn.style.width = "180px";
     }
     document.querySelectorAll("[data-i18n]").forEach((el) => {
         const key = el.getAttribute("data-i18n");
